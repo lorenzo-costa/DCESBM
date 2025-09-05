64fd6df5e3648f742f1f48ebd29851fcca9c64bd
Lorenzo Costa <lorenzocosta2001@gmail.com>
2025-09-05 16:11:34 -0400

    Added docstring and updated readme

--- README.md
+++ README.md
@@ -13,20 +13,12 @@ The codebase serves two main purposes:
 1. **Simulation Studies:** Scripts to reproduce the synthetic experiments in the thesis, demonstrating the performance of ESBMs under different network settings.
 2. **Goodreads Book Ratings Analysis:** An empirical study applying ESBM and its degree-corrected variant to a large-scale **Goodreads dataset** of book ratings.
 
+This repository provides a **reproducible workflow** for both simulation experiments and real-world analysis.
+
 ### Data
 The dataset is not stored in this repository due to its size. However:
 - A **script is provided** to preprocess and build the dataset from the raw files.
-- The raw Goodreads ratings data can be downloaded from [link to dataset].
-- Preprocessing includes filtering, network construction, and transformation into the bipartite graph structure used in this study.
-
-### Key Features
-- Bayesian nonparametric modeling of bipartite user-item networks.
-- Integration of **Gibbs-type priors** for flexible cluster assignment.
-- **Degree-correction** to model heterogeneity in user activity and item popularity.
-- Implementation of **MCMC algorithms** for posterior inference.
-- Tools for rating prediction, community structure estimation, and recommendation generation.
-
-This repository provides a **fully reproducible workflow** for both simulation experiments and real-world analysis.
+- The raw Goodreads ratings data can be downloaded from [this page](https://cseweb.ucsd.edu/~jmcauley/datasets/goodreads.html) (called 'goodreads_interactions_dedup.json').
 
 ### So... why does this matter? 
 Recommendation algorithms decide what we watch, read, or buy but they are often black boxes. This project aims to develop an interpretable algorithm modeling user-item interactions as networks. Instead of blindly factorizing a ratings matrix, we discover communities of similar users and items, making recommendations easier to interpret and explain. Plus, with degree correction, we don’t get fooled by power-users or ultra-popular items, your “hidden gem” recommendations stay hidden gems.
@@ -47,16 +39,44 @@ Recommendation algorithms decide what we watch, read, or buy but they are often 
 
 ## Installation
 Clone the repository and install requirements:
+(pip)
 ```bash
-git clone https://github.com/yourusername/repo-name.git
-cd repo-name
+git clone https://github.com/lorenzo-costa/REC-ESBM.git
+cd REC-ESBM
 pip install -r requirements.txt
+```
+(conda)
+```bash
+git clone https://github.com/lorenzo-costa/REC-ESBM.git
+cd repo-name
+conda env create -f environment.yml
+conda activate REC-ESBM
 ```
 
 ---
 
 ## Usage
-1. Preprocess the data:
+1. Preprocess the data: place Goodreads dataset in data/raw and run
 ```bash
 python src/pipeline/pre_processing_functs.py
 ``` 
+Note:
+- this is not needed for simulations
+- requires internet connection
+- may be quite slow
+
+2. Run simulations
+```bash
+python src/analysis/simulations.py\
+```
+3. Run data analysis:
+```bash
+python src/analysis/book_analysis.py
+```
+Note: this takes A LOT of time
+
+
+To run the whole analysis with one command run
+```bash
+make all
+```
--- src/pipeline/load_data.py
+++ src/pipeline/load_data.py
@@ -1,3 +1,8 @@
+
+#######################################
+# Script to extract data from gzip file, process it and save it as csv
+#######################################
+
 import json
 from collections import Counter
 from pre_processing_functs import process_data, find_relevant_ratings, process_books, load_data
@@ -8,10 +13,13 @@ interactions_DIR = 'data/raw/goodreads_interactions_dedup.json.gz'
 books_DIR = 'data/raw/goodreads_books.json.gz'
 works_DIR = 'data/raw/goodreads_book_works.json.gz'
 
-subset_size = 5000
-
+######################################
+# pick only the most read 1k books and most active 1k users
+#######################################
+subset_size = 1000
 out_users, out_books = process_data(interactions_DIR, stop=1e8, step = 1e6)
-    
+
+# merge counters for each batch
 out_users_cum = Counter()
 for x in out_users:
     out_users_cum = out_users_cum+x
@@ -27,28 +35,31 @@ for x in out_users:
 out_books_cum = Counter()
 for x in out_books:
     out_books_cum = out_books_cum+x
-    
+
+# use counters to find the most common users and books
 top_users = dict(out_users_cum.most_common(subset_size))
 top_books = dict(out_books_cum.most_common(subset_size))
 
 top_users_id = list(top_users.keys())
 top_books_id = list(top_books.keys())
 
+# extract ratings for the selected users and books
 selected_ratings, selected_uids, selected_bids = find_relevant_ratings(interactions_DIR, top_users_id, top_books_id, start=0, stop=1e8, step=1e6)
 
+# extract book information for the selected books
 out, out_book_id, out_description, out_work_id = process_books(books_DIR, top_books_id, start=0, stop=1e8, step=1e6)
 
-work_data = load_data(works_DIR, 0, 1e8)
+# create mapping from work_id to title and best_book_id
+work_data = load_data(works_DIR, start=0, end=1e8)
 work_id_title = {x['work_id']:x['original_title'] for x in work_data}
 work_id_best_book_id = {x['work_id']:x['best_book_id'] for x in work_data}
 
+# extract genres for the selected books
 genres = find_genres_parallel(out, max_workers=20)
 
 dt = pd.DataFrame({'rating':selected_ratings, 'user_id':selected_uids, 'book_id':selected_bids})
 other_dt = pd.DataFrame({'book_id':out_book_id, 'work_id':out_work_id, 'genres':genres})
 merged_dataset = pd.merge(dt, other_dt, how='left', on='book_id')
-
-#merged_dataset['genres'] = merged_dataset['genres'].apply(lambda x: x if isinstance(x, list) else [])
 
 merged_dataset['book_id'] = merged_dataset.apply(
     lambda row: work_id_best_book_id.get(row['work_id'], row['book_id']), axis = 1)
@@ -62,9 +73,10 @@ merged_dataset['fiction'] = merged_dataset['genres'].apply(lambda x: 1 if len(se
 merged_dataset['mistery'] = merged_dataset['genres'].apply(lambda x: 1 if len(set(x).intersection(['Mystery', 'mystery']))>0 else 0)
 merged_dataset['classic'] = merged_dataset['genres'].apply(lambda x: 1 if len(set(x).intersection(['Classic', 'classic', 'Classics', 'classics']))>0 else 0)
 
-merged_dataset.to_csv('data/processed/dataset_clean.csv')
 
 # save everything in data/processed
+merged_dataset.to_csv('data/processed/dataset_clean.csv')
+
 with open("data/processed/user_counts.json", "w") as f:
     json.dump(dict(out_users_cum), f)
     
